d10914fa9200f01117c7c0ca9b3df69d51a93756
AlexMD18 <AlexMD2918@gmail.com>
2021-01-10 13:23:37 -0500

    Add files via upload

---
+++ BookLibrary/Book.cs
@@ -0,0 +1,134 @@
+﻿/*
+Alex Drogo
+09/30/2020
+CIS 3342
+Prof. Pascucci
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using Utilities;
+
+namespace BookLibrary
+{
+    public class Book
+    {
+        //Private Objects
+        private string hiddenISBN;
+        private string hiddenTitle;
+        private string hiddenAuthor;
+        private float hiddenBasePrice;
+        private float hiddenTotalSales;
+        private int hiddenTotQuantityRented;
+        private int hiddenTotQuantitySold;
+        private string hiddenBookType;
+        private string hiddenRentBuy;
+        private int hiddenOrderQuantity;
+        private float hiddenTotalCost;
+
+        //Parameterless Constructor
+        public Book()
+        {
+            this.hiddenISBN = "";
+            this.hiddenTitle = "";
+            this.hiddenAuthor = "";
+            this.hiddenBasePrice = 0;
+            this.hiddenTotalSales = 0;
+            this.hiddenTotQuantityRented = 0;
+            this.hiddenTotQuantitySold = 0;
+            this.hiddenBookType = "";
+            this.hiddenRentBuy = "";
+            this.hiddenOrderQuantity = 0;
+            this.hiddenTotalCost = 0;
+        }
+
+        //Paramaterized Constructor
+        public Book(string isbn, string title, string author, float basePrice, float totalSales, int totQuantityRented, 
+            int totQuantitySold, string bookType, string rentBuy, int orderQuantity, float totalCost)
+        {
+            hiddenISBN = isbn;
+            hiddenTitle = title;
+            hiddenAuthor = author;
+            hiddenBasePrice = basePrice;
+            hiddenTotalSales = totalSales;
+            hiddenTotQuantityRented = TotalQuantityRented;
+            hiddenTotQuantitySold = totQuantitySold;
+            hiddenBookType = bookType;
+            hiddenRentBuy = rentBuy;
+            hiddenOrderQuantity = orderQuantity;
+            hiddenTotalCost = totalCost;
+        }
+
+        //Getters and setters
+        public string ISBN
+        {
+            get { return hiddenISBN; }
+            set { hiddenISBN = value; }
+        }
+
+        public string Title
+        {
+            get { return hiddenTitle; }
+            set { hiddenTitle = value; }
+        }
+
+        public string Author
+        {
+            get { return hiddenAuthor; }
+            set { hiddenAuthor = value; }
+        }
+
+        public float BasePrice
+        {
+            get { return hiddenBasePrice; }
+            set { hiddenBasePrice = value; }
+        }
+
+        public float TotalSales
+        {
+            get { return hiddenTotalSales; }
+            set { hiddenTotalSales = value; }
+        }
+
+        public int TotalQuantityRented
+        {
+            get { return hiddenTotQuantityRented; }
+            set { hiddenTotQuantityRented = value; }
+        }
+        
+        public int TotalQuantitySold
+        {
+            get { return hiddenTotQuantitySold; }
+            set { hiddenTotQuantitySold = value; }
+        }
+
+        public string BookType
+        {
+            get { return hiddenBookType; }
+            set { hiddenBookType = value; }
+        }
+
+        public string RentBuy
+        {
+            get { return hiddenRentBuy; }
+            set { hiddenRentBuy = value; }
+        }
+
+        public int OrderQuantity
+        {
+            get { return hiddenOrderQuantity; }
+            set { hiddenOrderQuantity = value; }
+        }
+
+        public float TotalCost
+        {
+            get { return hiddenTotalCost; }
+            set { hiddenTotalCost = value; }
+        }
+        //End of getters and setters
+
+    }
+}
---
+++ BookLibrary/CalcBookPrice.cs
@@ -0,0 +1,95 @@
+﻿/*
+Alex Drogo
+09/30/2020
+CIS 3342
+Prof. Pascucci
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Data;
+using Utilities;
+
+
+namespace BookLibrary
+{
+    public class CalcBookPrice
+    {
+        Book book = new Book();
+
+        //Find the final price of the order
+        public float calcPrice(float basePrice, int orderQuant)
+        {
+
+            float totCost = 0;
+
+            totCost = basePrice * orderQuant;
+
+            return totCost;
+        }
+
+        //Find new price of item after addons or deductions have taken place
+        public float newBasePrice(float basePrice, string rentBuy, string bookType)
+        {
+            float newBP = 0;
+
+            if (rentBuy == "Rent")
+            {
+                basePrice *= 0.5f;
+            }
+            else
+            {
+                basePrice *= 1f;
+            }
+
+            if (bookType == "Hardcover")
+            {
+                basePrice *= 1.5f;
+            }
+            else if (bookType == "E-Book")
+            {
+                basePrice *= 0.5f;
+            }
+            else
+            {
+                basePrice *= 1f;
+            }
+
+            newBP = basePrice;
+            return newBP;
+        }
+    }
+}
+
+
+
+
+
+/*if (book.RentBuy == "Rent")
+{
+    rentQuantity += book.OrderQuantity;
+    book.BasePrice *= 0.5f;
+}
+else
+{
+    buyQuantity += book.OrderQuantity;
+}
+
+    if (book.BookType == "Hardcover")
+{
+    book.BasePrice *= 1.5f;
+}
+    else if (book.BookType == "E-Book")
+{
+    book.BasePrice *= 0.5f;
+}
+    else               
+{
+    book.BasePrice *= 1f;
+}
+    book.TotalCost += (book.BasePrice * book.OrderQuantity);*/
